a569d43a7cfc69650df5ae4d04bf71e40c558751
prakashjashnani25 <prakashjashnani25@gmail.com>
2020-10-04 02:27:16 -0700

    Initial Commit

---
+++ python-30day/ex01.py
@@ -0,0 +1,23 @@
+str1="30 day 30 hour challenge"
+print(str1)
+days = "thirty"
+
+print(days[0])
+
+challenge="I will win"
+
+print(challenge[7:10])
+
+print("Length is "+str(len(challenge))+"")
+
+print("Convert to lower case "+challenge.lower())
+
+text = "Thirty Days and Thirty Hours"
+
+x = text.casefold();
+
+print("Orignal Text is - "+text+ " after Case Fold  "+x)
+
+print("Capital "+text.capitalize()+ " find 'Thi' "+str(text.find("Thi"))+" is alpha"+str(text.isalpha())+ " is all num" + str(text.isalnum()))
+
+
